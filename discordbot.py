93f192bc0f33472ffb64e4d55f5fc1fa6f005cb0
ftnext <takuyafjp+develop@gmail.com>
2021-09-05 19:11:08 +0900

    [feat] Staff can know that mogirin start (close #2)

--- discordbot.py
+++ discordbot.py
@@ -15,6 +15,16 @@ async def on_command_error(ctx, error):
     await ctx.send(error_message)
 
 
+async def greet(channel_id=int(getenv("LOGGING_CHANNEL_ID"))):
+    channel = bot.get_channel(channel_id)
+    await channel.send("[INFO] もぎりん、起動しました")
+
+
+@bot.event
+async def on_ready():
+    await greet()
+
+
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
