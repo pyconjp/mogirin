27e4f30bfd17bb7862c93ad791c41728e2d7936a
ftnext <takuyafjp+develop@gmail.com>
2021-09-05 20:10:24 +0900

    [feat] Respond to mention with ticket number

--- discordbot.py
+++ discordbot.py
@@ -30,5 +30,30 @@ async def ping(ctx):
     await ctx.send("pong")
 
 
+def collect_ticket(ticket_number: str):
+    return "Hello"
+
+
+@bot.event
+async def on_message(message):
+    # TODO: もぎり用チャンネルでなければ早期リターンする（もぎり用チャンネルはまだない）
+
+    if bot.user not in message.mentions:
+        return
+
+    # TODO: 正規表現で最初に見つけた数字にするとより簡潔に書けそう
+    ticket_number = message.content.split()[1]
+    if not ticket_number.isnumeric():
+        reply_message = (
+            f"{message.author.mention} ValueError: "
+            f"invalid ticket number {ticket_number!r}\n"
+            "Please input numeric ticket number instead."
+        )
+    else:
+        reply_message = collect_ticket(ticket_number)
+
+    await message.channel.send(reply_message)
+
+
 token = getenv("DISCORD_BOT_TOKEN")
 bot.run(token)
