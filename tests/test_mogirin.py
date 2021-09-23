dd812a1e6874dd86b6204641e93cffb3a37c3725
ftnext <takuyafjp+develop@gmail.com>
2021-09-23 20:26:51 +0900

    [test] query_already_collected (False case)

--- tests/test_mogirin.py
+++ tests/test_mogirin.py
@@ -40,13 +40,24 @@ class FindCellTicketSheetSearcherTestCase(TestCase):
 
 
 class QueryAlreadyCollectedTicketSheetSearcherTestCase(TestCase):
-    def test_return_true(self):
-        worksheet = MagicMock(spec=gspread.Worksheet)
-        sut = m.TicketSheetSearcher(worksheet)
-        cell = gspread.Cell(2, 1, "345678")
-        worksheet.cell.return_value = gspread.Cell(2, 3, "✅")
+    def setUp(self):
+        self.worksheet = MagicMock(spec=gspread.Worksheet)
+        self.sut = m.TicketSheetSearcher(self.worksheet)
 
-        actual = sut.query_already_collected(cell)
+    def test_return_true(self):
+        cell = gspread.Cell(2, 1, "345678")
+        self.worksheet.cell.return_value = gspread.Cell(2, 3, "✅")
+
+        actual = self.sut.query_already_collected(cell)
 
         self.assertTrue(actual)
-        worksheet.cell.assert_called_once_with(2, 3)
+        self.worksheet.cell.assert_called_once_with(2, 3)
+
+    def test_return_false(self):
+        cell = gspread.Cell(3, 1, "345679")
+        self.worksheet.cell.return_value = gspread.Cell(3, 3, "")
+
+        actual = self.sut.query_already_collected(cell)
+
+        self.assertFalse(actual)
+        self.worksheet.cell.assert_called_once_with(3, 3)
