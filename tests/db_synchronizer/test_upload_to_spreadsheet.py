20bf7281f26cb4fd862b7f99f98a273bd1153faa
ftnext <takuyafjp+develop@gmail.com>
2021-10-05 16:53:29 +0900

    [test] Continuous receipt number in spreadsheet

--- tests/db_synchronizer/test_upload_to_spreadsheet.py
+++ tests/db_synchronizer/test_upload_to_spreadsheet.py
@@ -12,3 +12,31 @@ class DifferenceTestCase(TestCase):
 
         expected = [(2, "a"), (3, "b"), (7, "d")]
         self.assertEqual(actual, expected)
+
+    def test_sheet_records_are_continuous(self):
+        participants = [(2, "a"), (3, "b"), (5, "c"), (7, "d")]
+        records = [
+            {
+                "receipt_number": 2,
+                "display_name": "a",
+                "collected": "",
+                "type": "スタッフ",
+            },
+            {
+                "receipt_number": 3,
+                "display_name": "b",
+                "collected": "✅",
+                "type": "スタッフ",
+            },
+            {
+                "receipt_number": 5,
+                "display_name": "c",
+                "collected": "",
+                "type": "スタッフ",
+            },
+        ]
+
+        actual = difference(participants, records)
+
+        expected = [(7, "d")]
+        self.assertEqual(actual, expected)
